5b0f5995c6cd7fd6fd84a2f146e649760aa279a4
Gillian <gillian.melo.franco17@gmail.com>
2025-11-17 21:56:38 -0300

    feat: add the `infra/database.js` abstraction

---
+++ infra/database.js
@@ -0,0 +1,27 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  let client = new Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB
+  });
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query
+}
