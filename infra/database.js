ae6e45cdfaea1dc996ab8e745196cfe63e99595d
Gillian <gillian.melo.franco17@gmail.com>
2025-11-21 17:54:55 -0300

    feat: require an SSL connection to the database when the project is in production

--- infra/database.js
+++ infra/database.js
@@ -1,14 +1,16 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  let client = new Client({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB
-  });
+  let client;
   try {
+    client = new Client({
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      database: process.env.POSTGRES_DB,
+      ssl: getSSLValues()
+    });
     await client.connect();
     const result = await client.query(queryObject);
     return result;
@@ -24,4 +26,11 @@ async function query(queryObject) {
 
 export default {
   query
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return { ca: process.env.POSTGRES_CA };
+  }
+  return (process.env.NODE_ENV === "production") ? true : false;
 }
